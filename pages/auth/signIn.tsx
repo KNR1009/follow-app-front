5d34fd521e452caae211009b694b62bc07e74215
伊東和成 <itokazunari5375@icloud.com>
2022-07-17 21:41:27 +0900

    フロントのログイン画面・新規作成画面を作成

---
+++ pages/auth/signIn.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import SignInSide from "../../components/auth/SignInSide";
+
+const SignIn: NextPage = () => {
+  return <SignInSide />;
+};
+
+export default SignIn;
---
+++ pages/auth/signUp.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import SignUpSide from "../../components/auth/SignUpSide";
+
+const SignUp: NextPage = () => {
+  return <SignUpSide />;
+};
+
+export default SignUp;
