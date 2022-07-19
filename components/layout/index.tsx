1fb512e4dafcbc2a18c6c3a8934a48cb08de050c
伊東和成 <itokazunari5375@icloud.com>
2022-07-19 23:08:24 +0900

    Layoutコンポーネントの作成

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,9 +1,17 @@
 import React, { ReactNode } from "react";
+import styled from "styled-components";
 
 type LayoutProps = {
   children: ReactNode;
 };
 
 export const Layout: React.FC<LayoutProps> = ({ children }) => {
-  return <div>{children}</div>;
+  return <LayoutContainer>{children}</LayoutContainer>;
 };
+
+const LayoutContainer = styled.div`
+  padding: 40px;
+  height: 100vh;
+  background-color: #000000;
+  color: #ffffff;
+`;
